507613844a65d172a5d0090a9786f03390cd8666
Ap0mano <apomanko1@gmail.com>
2023-01-16 21:32:45 +0100

    jhds

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"scraper/helper"
 )
 
+// switch to yummly
+
 func main() {
 	parsed := helper.GetData("https://www.bigoven.com/recipe/frosted-strawberry-salad/502652")
 	fmt.Println(parsed.Description)
